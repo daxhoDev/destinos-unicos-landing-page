1a25c82dfd0be0fdff000e3af6fb088f6020bba6
daxho <developer.daxho@gmail.com>
2025-07-23 11:41:48 -0400

    upgraded view

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,51 @@
 export default function Footer() {
   return (
     <footer
-      className="bg-gradient-to-r from-blue-500 via-blue-400 to-pink-400 text-white py-8 px-4 lg:px-8 text-center flex flex-col gap-6 lg:flex-row md:justify-between md:gap-8 mt-auto"
+      className="bg-blue-950 text-white py-8 px-4 lg:px-8 text-center flex flex-col gap-6 lg:flex-row md:justify-between md:gap-8 mt-auto"
       id="footer"
     >
       <div className="text-lg md:text-left">
-        <strong>Teléfono:</strong> +53 53540976
+        <strong>Teléfono:</strong>{" "}
+        <a
+          href="tel:+53 53540976"
+          className="hover:text-pink-200 transition-colors"
+        >
+          +5353540976
+        </a>
       </div>
       <div className="text-lg md:text-left">
-        <strong>Redes sociales:</strong> [Enlaces a redes sociales]
+        <strong>Redes sociales:</strong>
+        <ul className="flex flex-col gap-2">
+          <li>
+            <a
+              href="https://www.instagram.com/yaritzar8011?igsh=MTBscDF0bmw3OXJw"
+              className="hover:text-pink-200 transition-colors"
+            >
+              Instagram
+            </a>
+            <li>
+              <a
+                href="https://www.facebook.com/share/175uXcUorm/"
+                className="hover:text-pink-200 transition-colors"
+              >
+                Facebook
+              </a>
+            </li>
+          </li>
+        </ul>
       </div>
       <div className="text-lg md:text-left">
         <strong>Dirección:</strong> Calle 250 entre 25 y 27 Número 25A08
         Interior, San Agustín, La Lisa, La Habana, Cuba
       </div>
       <div className="text-lg md:text-left">
-        <strong>Correo electrónico:</strong> [Correo electrónico]
+        <strong>Correo electrónico:</strong>{" "}
+        <a
+          href="mailto:yrd261180@gmail.com"
+          className="hover:text-pink-200 transition-colors"
+        >
+          yrd261180@gmail.com
+        </a>
       </div>
     </footer>
   );
--- src/components/landing/HeroSection.jsx
+++ src/components/landing/HeroSection.jsx
@@ -6,14 +6,14 @@ export default function HeroSection() {
       className="h-dvh px-4 flex flex-col md:flex-row justify-center items-center gap-8 md:gap-12 text-center md:text-left bg-gradient-to-tl from-pink-400/90 via-blue-500/90 to-blue-400/90 bg-cover bg-center bg-no-repeat text-white"
       id="hero"
       style={{
-        backgroundImage: `linear-gradient(to top left, rgba(255, 111, 163, 0.9), rgba(58, 141, 222, 0.9) 60%), url(${background})`,
+        backgroundImage: `linear-gradient(to top left, rgba(255, 111, 163, 0.7), rgba(58, 141, 222, 0.7) 60%), url(${background})`,
       }}
     >
       <div>
-        <h1 className="text-3xl md:text-5xl font-bold mb-4 tracking-wider">
+        <h1 className="text-3xl md:text-5xl font-bold mb-4 tracking-wider text-shadow-lg">
           Destinos Únicos
         </h1>
-        <p className="text-lg md:text-2xl">
+        <p className="text-lg md:text-2xl text-shadow-lg">
           ¡Regalos y experiencias que dejan huella en cada ocasión!
         </p>
       </div>
