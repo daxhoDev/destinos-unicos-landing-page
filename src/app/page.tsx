4b155fe23faad700c5c6f8458d23aa017590e950
daxho <developer.daxho@gmail.com>
2026-03-13 15:08:35 -0400

    refactor: GiftsSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,13 @@
-"use client";
-
 import HeroSection from "@/components/heroSection";
 import GiftsSection from "@/components/giftsSection";
 import AboutSection from "@/components/aboutSection";
 import ReviewsSection from "@/components/reviewsSection";
-import { gifts } from "@/data";
-import GiftCard from "@/components/giftCard";
-import { useState } from "react";
-import { Gift } from "@/types";
-import GiftModal from "@/components/giftModal";
 
 export default function Home() {
-  const [selectedGift, setSelectedGift] = useState<Gift | null>(null);
-
-  function handleSelectGift(gift: Gift) {
-    setSelectedGift(gift);
-  }
-
-  function handleCloseModal() {
-    setSelectedGift(null);
-  }
-
   return (
     <main>
       <HeroSection />
-      <GiftsSection>
-        {gifts.slice(0, 3).map((gift) => (
-          <GiftCard
-            key={gift.id}
-            gift={gift}
-            onSelectGift={() => handleSelectGift(gift)}
-          />
-        ))}
-        {selectedGift && (
-          <GiftModal gift={selectedGift} onCloseModal={handleCloseModal} />
-        )}
-      </GiftsSection>
+      <GiftsSection />
       <AboutSection />
       <ReviewsSection />
     </main>
--- src/components/GiftsSection.tsx
+++ src/components/GiftsSection.tsx
@@ -1,11 +1,24 @@
+"use client";
+
 import Button from "@/components/button";
 import Link from "next/link";
+import { useState } from "react";
+import { Gift } from "@/types";
+import GiftModal from "./giftModal";
+import { gifts } from "@/data";
+import GiftCard from "./giftCard";
 
-export default function GiftsSection({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function GiftsSection() {
+  const [selectedGift, setSelectedGift] = useState<Gift | null>(null);
+
+  function handleSelectGift(gift: Gift) {
+    setSelectedGift(gift);
+  }
+
+  function handleCloseModal() {
+    setSelectedGift(null);
+  }
+
   return (
     <section
       className="py-8 px-4 text-center bg-blue-50 flex flex-col items-center"
@@ -15,11 +28,20 @@ export default function GiftsSection({
         Nuestros Regalos Destacados
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full max-w-3xl mb-8">
-        {children}
+        {gifts.slice(0, 3).map((gift) => (
+          <GiftCard
+            key={gift.id}
+            gift={gift}
+            onSelectGift={() => handleSelectGift(gift)}
+          />
+        ))}
       </div>
       <Link href="/catalog">
         <Button>Ver catálogo completo</Button>
       </Link>
+      {selectedGift && (
+        <GiftModal gift={selectedGift} onCloseModal={handleCloseModal} />
+      )}
     </section>
   );
 }
