1ed6961e271de833a7aec2d1b65fb232f40f6cae
daxho <developer.daxho@gmail.com>
2026-03-13 14:58:50 -0400

    chore: integrate supabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,11 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import "@/styles/globals.css";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html>
       <body>
