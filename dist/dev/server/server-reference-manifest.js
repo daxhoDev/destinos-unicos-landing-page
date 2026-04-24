e07d8ce4fa40052c3b46808b2c80a5b77dd6c6bd
daxho <developer.daxho@gmail.com>
2026-04-24 12:41:33 -0400

    chore: output build folder

---
+++ dist/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JJocNYvcfYf8cJWa0L0QYUQZJXyRrWfg3fnjQwhTEXw=\"\n}"
---
+++ dist/dev/static/chunks/src_app_layout_tsx_79775223._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_6241f9c3._.js",
+  "static/chunks/src_styles_globals_47180df9.css"
+],
+    source: "dynamic"
+});
